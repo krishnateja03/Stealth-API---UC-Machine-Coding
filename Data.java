a36715212a9551b1a2def32af1a622b2ac8f7b49
krishnateja03 <tejakchenchala@gmail.com>
2022-04-03 19:57:30 +0530

    added data model object

---
+++ Data.java
@@ -0,0 +1,11 @@
+package StealthAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Data {
+    private int jobId;
+    private int value;
+}
